3136342b21d71145b4985b596b2752e2af252c26
N H <nh@MBP.local>
2025-01-28 21:35:54 +0900

    root layout、定義ファイル作成

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_NAME, APP_DESCRIPTIOM, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "EC Site",
-  description: "A modern ecommerce platform built with Next.js",
+  title: { template: `%s | ${APP_NAME}`, default: APP_NAME },
+  description: APP_DESCRIPTIOM,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "EC Site";
+export const APP_DESCRIPTIOM =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTIOM ||
+  "A modern ecommerce platform built with Next.js";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
