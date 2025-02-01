17c28fd0668318eabc16b19b7b33ff1957860380
N H <nh@MBP.local>
2025-02-01 20:19:17 +0900

    ベースになるレイアウト作成

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-  return <>EC Site</>;
+  return (
+    <ProductList data={sampleData.products} title="Newest Arrival" limit={4} />
+  );
 };
 
 export default Homepage;
